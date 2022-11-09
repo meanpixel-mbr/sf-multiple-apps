89be9e75263ce13724c62c55a1390815efb0574c
Michael Brotrück <michael.brotrueck@rds.de>
2022-11-09 10:57:59 +0100

    initial

---
+++ SF.Common/SF.Common.Interfaces/Products/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using SF.Common.Poco.Products;
+
+namespace SF.Common.Interfaces.Products;
+
+public interface IProductService : IService
+{
+	Task<List<Product>> GetAsync(CancellationToken cancellationToken);
+	Task<Product> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ SF.Common/SF.Common.Poco/Products/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace SF.Common.Poco.Products;
+
+public class Product
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+
+}
